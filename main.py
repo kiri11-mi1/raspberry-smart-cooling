32d40c4627f176ae5a930a069d465ee77044dcdb
kiri11-mi1 <kirill.mil.0310@mail.ru>
2022-12-24 03:10:29 +0700

    refactor

--- main.py
+++ main.py
@@ -18,27 +18,28 @@ DELTA_TEMP = args.delta_temp
 
 
 def main(fan: Fan):
-	while True:
-		curr_temp = get_temp()
-		if is_good_time(TIME_BEGIN, TIME_END):
-			if curr_temp > MAX_TEMPERATURE and not fan.is_enable:
-				fan.enable()
-			elif curr_temp < MAX_TEMPERATURE - DELTA_TEMP and fan.is_enable:
-				fan.disable()
-		else:
-			fan.disable()
+    while True:
+        curr_temp = get_temp()
+        if is_good_time(TIME_BEGIN, TIME_END):
+            if curr_temp > MAX_TEMPERATURE and not fan.is_enable:
+                fan.enable()
+            elif curr_temp < MAX_TEMPERATURE - DELTA_TEMP and fan.is_enable:
+                fan.disable()
+        else:
+            fan.disable()
 
-		logging.info(f'| Fan state: {fan.is_enable} | Current temp: {curr_temp} |')
-		sleep(1)
+        logging.info(f'| Fan state: {fan.is_enable} | Current temp: {curr_temp} |')
+        sleep(1)
+
 
 if __name__ == '__main__':
-	f = Fan(CONTROL_PIN)
-	try:
-		main(f)
-	except KeyboardInterrupt:
-		logging.warning('Script is turned off')
-	except Exeption as e:
-		logging.error(f'Other exeption: {e}')
-	finally:
-		logging.info('Fan is turned off')
-		del f
+    f = Fan(CONTROL_PIN)
+    try:
+        main(f)
+    except KeyboardInterrupt:
+        logging.warning('Script is turned off')
+    except Exception as e:
+        logging.error(f'Other exeption: {e}')
+    finally:
+        logging.info('Fan is turned off')
+        del f
--- service.py
+++ service.py
@@ -5,18 +5,18 @@ from datetime import datetime
 
 
 def get_temp() -> float:
-	'''Получение текущей температуры в градусах'''
-	out_of_program = check_output(["vcgencmd","measure_temp"]).decode()
-	temp_value = findall('\d+\.\d+', out_of_program)[0]
-	return float(temp_value)
+    """Получение текущей температуры в градусах"""
+    out_of_program = check_output(["vcgencmd","measure_temp"]).decode()
+    temp_value = findall(r'\d+\.\d+', out_of_program)[0]
+    return float(temp_value)
 
 
 def is_good_time(begin: int, end: int) -> bool:
-	'''Можно лю включить вентилятор в данный момент'''
-	current = datetime.now().hour
-	if begin < end:
-		return end > current > begin
-	elif begin > end:
-		interval = list(range(begin, 24)) + list(range(end))
-		return current in interval
-	return False
+    """Можно лю включить вентилятор в данный момент"""
+    current = datetime.now().hour
+    if begin < end:
+        return end > current > begin
+    elif begin > end:
+        interval = list(range(begin, 24)) + list(range(end))
+        return current in interval
+    return False
